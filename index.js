db6fbf90b18f7fb05653e8de8dc75ad41d86b147
Abhishekjha12 <abhijha218@gmail.com>
2025-08-16 02:25:55 +0530

    Prepare for Render deployment

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const port = 4000
+
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/twitter',(req,res)=>{
-    res.send('abhishek jha')
-})
-app.get('/login',(req,res)=>{
-    res.send('<h1>login here for twitter</h1>')
-})
-app.get('/youtube',(req,res)=>{
-    res.send('<h2>welcome to youtube</h2>')
-})
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+  res.send('abhishek jha');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>login here for twitter</h1>');
+});
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2>welcome to youtube</h2>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
